653d4a9a22b0dff8b4be65cd203d5265aa8a6850
bbbruce <bbbruce@emory.edu>
2023-12-08 09:57:11 -0500

    07 post part 2

--- 07/07.R
+++ 07/07.R
@@ -12,18 +12,37 @@ d$hand %>%
     strsplit("") %>%
     map(table) %>%
     map(sort) %>%
-    map(rev) %>%
+    map(rev) %>% 
+    map(~ {
+        if(!is.na(.["J"]))
+            if(names(.)[1] == "J") {
+                if(.[1] == 5) {
+                    names(.)[1] <- "A"
+                } else {
+                    .[2] <- .[1] + .[2]
+                }
+            } else {
+                .[1] <- .[1] + .["J"]
+            }
+            .["J"] <- 0
+        .}) %>%
+    map(~ keep(., . > 0)) %>%
     map_chr(~ sprintf("%-5s", paste(., collapse = ""))) ->
     d$handrank
 
 d$hand %>%
-    map(~ chartr("AKQJT98765432", "EDCBA98765432", .)) ->
+    map(~ chartr("AKQJT98765432", "EDC1A98765432", .)) ->
     d$lexohand
 
 d %>%
     mutate(handsort = paste0(handrank, lexohand)) %>%
     arrange(desc(handsort)) %>%
     mutate(rank = rev(1:NROW(.)),
-        winnings = rank * as.numeric(bid)) %>%
+        winnings = rank * as.numeric(bid)) ->
+    d
+    
+d %>%
         pull(winnings) %>%
         sum()
+
+# 249089358 low
