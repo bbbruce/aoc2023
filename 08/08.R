a391f773afa49bdbe3640aaec083f3c1b4d7f2a8
bbbruce <bbbruce@emory.edu>
2023-12-08 15:09:19 -0500

    08 post part 2

--- 08/08.R
+++ 08/08.R
@@ -11,23 +11,39 @@ for (i in 3:length(input)) {
 }
 
 where <- ls(envir = e) %>% grep("A$", ., value = TRUE)
-step <- 0
-i <- 1
-while(TRUE) {
-    lr <- map(where, ~ get(., envir = e))
-    if (dir[i] == "L") {
-        where <- map_chr(lr, 1)
-    } else {
-        where <- map_chr(lr, 2)
+
+process_it <- function(where, end = "Z$", d = dir) {
+    dir <- d
+    step <- 0
+    i <- 1
+    while(TRUE) {
+        lr <- map(where, ~ get(., envir = e))
+        if (dir[i] == "L") {
+            where <- map_chr(lr, 1)
+        } else {
+            where <- map_chr(lr, 2)
+        }
+        step <- step + 1
+        if (all(grepl(end, where))) {
+            break
+        }
+        i <- i + 1
+        if (i > length(dir)) {
+            i <- 1
+        }
     }
-    step <- step + 1
-    if (all(grepl("Z$", where))) {
-        break
-    }
-    i <- i + 1
-    if (i > length(dir)) {
-        i <- 1
-    }
+    step
 }
 
-step
+to_zs <- map_int(where, process_it)
+
+library(gmp)
+map(to_zs, as.bigz) -> bz
+bzlcm <- bz[[1]]
+for (i in 2:length(bz)) {
+    bzlcm <- lcm.bigz(bzlcm, bz[[i]])
+}
+
+# 16722109789192057789770047 <- too big
+# 13289612809129 <- correct!
+# brute force had only made it to step 191804702 after many hours... 
